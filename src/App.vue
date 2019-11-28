e92df09695d7d6f18a9fc8ee2a4748a2da63f4c4
Uuganbaatar <uuganbaatarbaynsuren2016@gmail.com>
2019-11-28 13:48:10 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-content>
+      <W9Form/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import W9Form from './components/W9Form';
 
 export default {
-  name: 'app',
+  name: 'App',
+
   components: {
-    HelloWorld,
+    W9Form,
   },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/W9Form.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-form
+    ref="form"
+    v-model="valid"
+    class="irs-form mt-10">
+    <v-container>
+      <v-layout
+        column
+        wrap>
+        <v-flex>
+          <h1 class="headline">Form W-9</h1>
+          <p class="py-4">
+            Please see the <a href="#" class="irs-link">instructions from the IRS</a>
+            to fill out this form
+          </p>
+        </v-flex>
+        <v-flex xs6>
+          <v-text-field
+            v-model="name"
+            :rules="nameRules"
+            label="1. Name (as shown on your income tax return)"
+            placeholder=" "
+            required>
+          </v-text-field>
+          <v-btn class="mt-4" :disabled="!valid" @click="submit">submit</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'W9Form',
+
+  data: () => ({
+    valid: false,
+    name: '',
+    nameRules: [
+      v => !!v || 'Name is required!',
+    ],
+  }),
+
+  methods: {
+    submit() {
+      if (this.$refs.form.validate()) {
+        alert('Submit');
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.irs-form {
+  p {
+    color: #707070;
+  }
+
+  a.irs-link {
+    color: #000000;
+  }
+
+  .v-label {
+    font-size: 1.2em;
+    color: rgba(0, 0, 0, 0.87)!important;
+  }
+}
+
+</style>
